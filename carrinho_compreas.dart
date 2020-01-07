fe73080abd8ac83ef28bf92767be0a905841126f
Keylane Silva <keylanessilva07@gmail.com>
2020-01-07 17:24:00 -0300

    /

--- carrinho_compreas.dart
+++ carrinho_compreas.dart
@@ -1,40 +1,65 @@
 import 'dart:io';
 
-List<String> produtos = [];
-main() {
-  
-  
+  List<String> produtos = [];
   bool condicao = true;
+  int opc;
 
-  while(condicao){
-    print("===== ADICIONE UM PRODUTO =====");
-    String text = stdin.readLineSync();
+inserir(){
+  print("Digite o nome do produtos que deseja inserir");
+  String prod = stdin.readLineSync();
+  produtos.add(prod);
+  print("\x1B[2J\x1B[0;0H");
+  print("PRODUTO INSERIDO COM SUCESSO!\n");
+}
 
-    if(text == "sair"){
-      print("===== TERMINOU O PROGRAMA =====");
-      condicao = false;
-    }else if(text == "imprimir"){
-      imprimir();
-    }else if(text == "remover"){
-      remover();
-    }else{
-      produtos.add(text);
-      print("\x1B[2J\x1B[0;0H");
-    }
-  }
-
+remover(){
+  print("Qual item deseja remover?");
+    imprimir();
+    int item = int.parse(stdin.readLineSync());
+    produtos.removeAt(item);
+    print("ITEM REMOVIDO");
+    print("\n");
 }
 
 imprimir(){
   for (var i = 0; i < produtos.length; i++) {
     print("ITEM $i - ${produtos[i]}");
   }
+  print("\n");
 }
 
-remover(){
-  print("Qual item deseja remover?");
-      imprimir();
-      int item = int.parse(stdin.readLineSync());
-      produtos.removeAt(item);
-      print("ITEM REMOVIDO");
+main() {
+  
+  print("===== SEJA BEM-VINDO ====\n");
+  print("===== ESCOLHA UMA DAS OPÇÕES =====\n");
+  
+  do {
+    print(" 1-INSERIR PRODUTO\n 2-REMOVER PRODUTO\n 3-MOSTRAR PRODUTOS\n 0-SAIR");
+    opc = int.parse(stdin.readLineSync());
+    
+    switch (opc) {
+      case 1:
+      {
+        inserir();
+      }break;
+      case 2:
+      {
+        remover();
+      }break;
+      case 3:
+      {
+        imprimir();
+      }break;
+      default:
+      {
+        if(opc!=0){
+          print("ENTRADA INVÁLIDA!\n");
+        }else{
+          opc = 0;
+        }
+      }  
+    }
+  } while (opc!=0);
+  print("====== PROGRAMA FINALIZADO ======");
 }
+
